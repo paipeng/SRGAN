f845a071ab198f1f22801d11a2be5edb42c7e1dd
Pai Peng <sipaipv6@gmail.com>
2023-01-10 16:22:58 +0800

    gauss blur size 15

--- data_utils.py
+++ data_utils.py
@@ -37,7 +37,7 @@ def train_lr_transform(crop_size, upscale_factor, gray=False):
             #Resize(crop_size // upscale_factor, interpolation=InterpolationMode.BILINEAR),
             Resize(crop_size, interpolation=InterpolationMode.BILINEAR),
             Grayscale(num_output_channels=1),
-            GaussianBlur(5),
+            GaussianBlur(15),
             ToTensor()
         ])
     else:
@@ -45,7 +45,7 @@ def train_lr_transform(crop_size, upscale_factor, gray=False):
             ToPILImage(),
             #Resize(crop_size // upscale_factor, interpolation=InterpolationMode.BILINEAR),
             Resize(crop_size, interpolation=InterpolationMode.BILINEAR),
-            GaussianBlur(5),
+            GaussianBlur(15),
             ToTensor()
         ])
 
