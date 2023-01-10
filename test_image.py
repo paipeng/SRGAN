f0617187df7e414223d36063739f79a9d65569de
Pai Peng <sipaipv6@gmail.com>
2023-01-10 19:27:19 +0800

    crop argument

--- test_image.py
+++ test_image.py
@@ -4,7 +4,7 @@ import time
 import torch
 from PIL import Image
 from torch.autograd import Variable
-from torchvision.transforms import ToTensor, ToPILImage
+from torchvision.transforms import ToTensor, ToPILImage, InterpolationMode
 
 from model import Generator
 
@@ -13,6 +13,7 @@ parser.add_argument('--upscale_factor', default=4, type=int, help='super resolut
 parser.add_argument('--test_mode', default='CPU', type=str, choices=['GPU', 'CPU'], help='using GPU or CPU')
 parser.add_argument('--image_name', type=str, required=True, help='test low resolution image name')
 parser.add_argument('--model_name', default='netG_epoch_2_100.pth', type=str, help='generator model epoch name')
+parser.add_argument('-c', '--crop', default=False, type=bool, help='crop image')
 opt = parser.parse_args()
 
 UPSCALE_FACTOR = opt.upscale_factor
@@ -28,6 +29,19 @@ else:
     model.load_state_dict(torch.load(MODEL_NAME, map_location=lambda storage, loc: storage))
 print(f'input image name: {IMAGE_NAME}')
 image = Image.open(IMAGE_NAME)
+if opt.crop:
+    image = image.resize((640, 640), interpolation=InterpolationMode.BILINEAR)
+    new_width = 432
+    new_height = 360
+    width, height = image.size   # Get dimensions
+    left = (width - new_width)/2
+    top = (height - new_height)/2
+    right = (width + new_width)/2
+    bottom = (height + new_height)/2
+
+    # Crop the center of the image
+    image = image.crop((left, top, right, bottom))
+
 
 with torch.no_grad():
     image = Variable(ToTensor()(image), volatile=True).unsqueeze(0)
