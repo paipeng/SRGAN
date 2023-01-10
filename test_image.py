aa0d2a36c3eda470461a568e5f1d6c50fe0f7bf1
Pai Peng <sipaipv6@gmail.com>
2023-01-10 16:03:08 +0800

    update

--- test_image.py
+++ test_image.py
@@ -23,9 +23,9 @@ MODEL_NAME = opt.model_name
 model = Generator(UPSCALE_FACTOR).eval()
 if TEST_MODE:
     model.cuda()
-    model.load_state_dict(torch.load('epochs/' + MODEL_NAME))
+    model.load_state_dict(torch.load(MODEL_NAME))
 else:
-    model.load_state_dict(torch.load('epochs/' + MODEL_NAME, map_location=lambda storage, loc: storage))
+    model.load_state_dict(torch.load(MODEL_NAME, map_location=lambda storage, loc: storage))
 
 image = Image.open(IMAGE_NAME)
 image = Variable(ToTensor()(image), volatile=True).unsqueeze(0)
